1fd1c60d4704abcacf443898a2025ba6b7e59bdf
Rodrigo Rivas Costa <rodrigorivascosta@gmail.com>
2023-12-09 16:38:14 +0100

    Add proper example

---
+++ easy-imgui/examples/demo.rs
@@ -0,0 +1,29 @@
+#![allow(unused_variables, unused_mut)]
+
+use easy_imgui_window::{MainWindow, MainWindowWithRenderer,
+    winit::event_loop::EventLoopBuilder,
+    easy_imgui as imgui,
+};
+
+fn main() {
+    let event_loop = EventLoopBuilder::new().build().unwrap();
+    let mut main_window = MainWindow::new(&event_loop, "Example").unwrap();
+    let mut window = MainWindowWithRenderer::new(main_window);
+
+    let mut app = App;
+
+    event_loop.run(move |event, w| {
+        let res = window.do_event(&mut app, &event, w);
+        if res.is_break() {
+            w.exit();
+        }
+    }).unwrap();
+}
+
+struct App;
+
+impl imgui::UiBuilder for App {
+    fn do_ui(&mut self, ui: &imgui::Ui<Self>) {
+        ui.show_demo_window(None);
+    }
+}
